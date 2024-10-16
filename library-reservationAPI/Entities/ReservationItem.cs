a7aa931d2b11438ed71bcc2752bed55adb4a8fda
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2024-10-17 01:05:41 +0300

    feat: add ReservationItem entity.

---
+++ library-reservationAPI/Entities/ReservationItem.cs
@@ -0,0 +1,22 @@
+﻿using library_reservationAPI.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace library_reservationAPI.Entities
+{
+    public class ReservationItem
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+
+        [Required]
+        [RegularExpression("(book|audiobook)", ErrorMessage = "Type must be either 'book' or 'audiobook'")]
+        public string Type { get; set; }
+
+        [Range(Constants.MIN_RESERVE_DAYS, Constants.MAX_RESERVE_DAYS, ErrorMessage = "Days to reserve must be within the allowed range")]
+        public int Days { get; set; }
+        public bool QuickPickUp { get; set; }
+
+
+    }
+}
---
+++ library-reservationAPI/Helpers/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace library_reservationAPI.Helpers
+{
+    static class Constants
+    {
+        public const string BOOK_TYPES = "book|audiobook";
+        public const int MAX_RESERVE_DAYS = 180;
+        public const int MIN_RESERVE_DAYS = 1;
+    }
+}
