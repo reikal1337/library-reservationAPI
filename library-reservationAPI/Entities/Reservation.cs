7994ad4f8873571640d45bd389e9f4d206321b1c
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2024-10-17 01:15:53 +0300

    feat: Reservation entity.

---
+++ library-reservationAPI/Entities/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace library_reservationAPI.Entities
+{
+    public class Reservation
+    {
+
+
+        public int Id { get; set; }
+
+        public int[] ItemIds { get; set; }
+
+        public ICollection<ReservationItem> ReservationItems { get; set; } = new List<ReservationItem>();
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal  TotalPrice { get; set; }
+    }
+}
--- library-reservationAPI/Entities/ReservationItem.cs
+++ library-reservationAPI/Entities/ReservationItem.cs
@@ -13,9 +13,12 @@ namespace library_reservationAPI.Entities
         [RegularExpression("(book|audiobook)", ErrorMessage = "Type must be either 'book' or 'audiobook'")]
         public string Type { get; set; }
 
-        [Range(Constants.MIN_RESERVE_DAYS, Constants.MAX_RESERVE_DAYS, ErrorMessage = "Days to reserve must be within the allowed range")]
+        [Range(Constants.MIN_RESERVE_DAYS, Constants.MAX_RESERVE_DAYS, ErrorMessage = "Days to reserve must be within the allowed range 1-180")]
         public int Days { get; set; }
         public bool QuickPickUp { get; set; }
+
+        public int ReservationId { get; set; }
+        public Reservation Reservation { get; set; }
 
 
     }
