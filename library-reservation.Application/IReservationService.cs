4ce508db4925404c9279817bd49feac46099b61d
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2024-10-17 21:40:20 +0300

    fix: iReservation had wrong return.

--- library-reservation.Application/IReservationService.cs
+++ library-reservation.Application/IReservationService.cs
@@ -8,7 +8,7 @@ namespace library_reservation.Application
     public interface IReservationService
     {
         Task<(List<Reservation>, int TotalRecords)> GetPaginatedReservations(PaginationDTO paginationDTO);
-        int GetReservationPrice(List<ReservationItemDTO> items);
+        decimal GetReservationPrice(List<ReservationItemDTO> items);
 
     }
 }
