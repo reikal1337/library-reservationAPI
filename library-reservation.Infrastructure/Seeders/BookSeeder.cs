c164dd0c191c84a380d4bdfc98d70351289ebeb1
LAPTOP-5Q44CSU7\reika <reikal951@gmail.com>
2024-10-17 16:54:55 +0300

    feat: add custom BadRequest parse.

--- library-reservation.Infrastructure/Seeders/BookSeeder.cs
+++ library-reservation.Infrastructure/Seeders/BookSeeder.cs
@@ -27,7 +27,14 @@ namespace library_reservation.Infrastructure.Seeders
                 new Book { Id = 2, Name = "Ulysses", ImageSrc = "https://images.thegreatestbooks.org/sbd37b2dsyuw15cv63l87biw63kv", Year = 1922, Type = new[] { "book", "audiobook" } },
                 new Book { Id = 3, Name = "In Search of Lost", ImageSrc = "https://images.thegreatestbooks.org/myvbhitdua7h1etye2hvfjej2p4j", Year = 1913, Type = new[] { "book" } },
                 new Book { Id = 4, Name = "One Hundred Years of Solitude", ImageSrc = "https://images.thegreatestbooks.org/fzce7ac1jcmx6fi8ppnea65ct3u9", Year = 1967, Type = new[] { "book" } },
-                new Book { Id = 5, Name = "The Catcher in the Rye", ImageSrc = "https://images.thegreatestbooks.org/azrr7j37ttxak2wmpmdoczhvd36t", Year = 1951, Type = new[] { "book" } }
+                new Book { Id = 5, Name = "The Catcher in the Rye", ImageSrc = "https://images.thegreatestbooks.org/azrr7j37ttxak2wmpmdoczhvd36t", Year = 1951, Type = new[] { "book" } },
+                new Book { Id = 6, Name = "Lolita", ImageSrc = "https://images.thegreatestbooks.org/t9epsngv7thxsjalsswqzgtnzlpu", Year = 1955, Type = new[] { "book" } },
+                new Book { Id = 7, Name = "Nineteen Eighty Four", ImageSrc = "https://images.thegreatestbooks.org/oka7y4u1r23osldhnx42q9lvxtpz", Year = 1949, Type = new[] { "book" } },
+                new Book { Id = 8, Name = "Anna Karenina", ImageSrc = "https://images.thegreatestbooks.org/m8kb7ah2lhy960dbp473zna11wb4", Year = 1877, Type = new[] { "book" } },
+                new Book { Id = 9, Name = "Moby-Dick", ImageSrc = "https://images.thegreatestbooks.org/4zdrnqgrbobtunihoxvad8kxotot", Year = 1851, Type = new[] { "book" } },
+                new Book { Id = 10, Name = "Wuthering Heights", ImageSrc = "https://images.thegreatestbooks.org/8lg4exp38r6236mjmp8nkx4k3k2j", Year = 1847, Type = new[] { "book" } },
+                new Book { Id = 11, Name = "Pride and Prejudice", ImageSrc = "https://images.thegreatestbooks.org/hfiptkzo82hkprq8p4yfxoxb8pns", Year = 1813, Type = new[] { "book" } },
+
             ];
 
             return books;
--- library-reservationAPI/Program.cs
+++ library-reservationAPI/Program.cs
@@ -2,18 +2,13 @@ using library_reservation.Infrastructure.Extensions;
 using library_reservation.Application;
 using library_reservation.Infrastructure.Repositories;
 using library_reservation.Infrastructure.Seeders;
+using NET_core_api_tut.Filters;
+using NET_core_api_tut.APIBehavior;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("DbConnection"))
-//    );
-
-
-
+//Infrastructure init.
 builder.Services.AddInfrastructure(builder.Configuration);
 
 
@@ -21,7 +16,11 @@ builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(option =>
+    {
+        option.Filters.Add(typeof(ParseBadRequest));
+    }
+).ConfigureApiBehaviorOptions(BadRequestsBahavior.Parse);
 
 
 var app = builder.Build();
